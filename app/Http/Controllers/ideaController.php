091da535f10184d6623c463a1070ed731d1fb6c1
CourtneyFradreck <courtaga@gmail.com>
2024-04-24 17:02:34 +0200

    added comments explaining the code

--- app/Http/Controllers/ideaController.php
+++ app/Http/Controllers/ideaController.php
@@ -11,13 +11,13 @@ class ideaController extends Controller
         
         request()->validate([
             'yourIdea' => 'required|min:5|max:1000|'
-        ]);
+        ]);//idea char validation
 
         $idea = idea::create([
             'content' => request()->get('yourIdea', null),
-        ]);
+        ]);//create yourIdea
 
         $idea->save();
-        return redirect()->route('dashboard')->with('success', 'Idea created succesfully!!') ;
+        return redirect()->route('dashboard')->with('success', 'Idea created succesfully!!') ;//displays a succes message if the idea was created succesfully
     }
 }
